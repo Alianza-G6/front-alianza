db1fa58f62ee3dc8765988532590dffa05885325
yEllenCaroline <ellen.chaves@sptech.school>
2024-10-16 19:35:20 -0300

    subindo cadastro de Funcionario

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -225,10 +225,51 @@ function cadastrar(req, res) {
     }
 }
 
+function cadastrarFunc(req, res) {
+   
+    var nome = req.body.nomeServer;
+    var email = req.body.emailServer;
+    var senha = req.body.senhaServer;
+    var tipoUsuario = req.body.tipoUserServer;
+    var fkEmpresa = req.body.fkEmpresaServer;
+
+    // Faça as validações dos valores
+    if (nome == undefined) {
+        res.status(400).send("Seu nome está undefined!");
+    } else if (email == undefined) {
+        res.status(400).send("Seu email está undefined!");
+    } else if (senha == undefined) {
+        res.status(400).send("Sua senha está undefined!");
+    } else if (tipoUsuario == undefined) {
+        res.status(400).send("Seu tipoUsuario está undefined!");
+    } else if (fkEmpresa == undefined) {
+        res.status(400).send("Sua fkEmpresa está undefined!");
+    } else {
+
+        // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
+        usuarioModel.cadastrar(nome, email, senha, tipoUsuario, fkEmpresa)
+            .then(
+                function (resultado) {
+                    res.json(resultado);
+                }
+            ).catch(
+                function (erro) {
+                    console.log(erro);
+                    console.log(
+                        "\nHouve um erro ao realizar o cadastro! Erro: ",
+                        erro.sqlMessage
+                    );
+                    res.status(500).json(erro.sqlMessage);
+                }
+            );
+    }
+}
+
 module.exports = {
     autenticar,
     emailNovo,
     cadastrar,
+    cadastrarFunc,
     senhaNova,
     deletarConta
 }
