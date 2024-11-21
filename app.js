e9a3a49356e16bb0beda2fc57334f6e47d4234bd
Moisés Silva <moises.silva@sptech.school>
2024-11-21 12:08:22 -0300

    adicionando rota no app.js

--- app.js
+++ app.js
@@ -20,12 +20,17 @@ var indexRouter = require("./src/routes/index");
 var usuarioRouter = require("./src/routes/usuarios");
 var feedbackRouter = require("./src/routes/feedbacks");
 var vooRouter = require("./src/routes/voos");
-
+var auth = require('./routes/auth');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(cors());
+
+
+
+app.use('/auth', auth);
+
 
 app.use("/", indexRouter);
 app.use("/usuarios", usuarioRouter);
